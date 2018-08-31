7d011085b214d35a73a76dd9c60dfc809f136d43
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-08-30 19:04:59 -0700

    added basic event listeners, movement indicators, and accel handlers

--- includes/station.js
+++ includes/station.js
@@ -7,10 +7,62 @@ class StationSim{
 			station: $(stationElement)
 		}
 		this.updateTimer = null;
-		this.startUpdater();
+		this.thrustPerInterval = 10;
+		this.thrust = {
+			x: 0,
+			y: 0
+		}
+		this.motion = {
+			x: 0,
+			y: 0
+		}
+		
 		this.updateIntervalAmount = 1000;
+		this.startUpdater();
+		this.setEventListeners();
 	}
-
+	setEventListeners(){
+		$('body').on('keyup', this.handleKeyUp.bind(this))
+		$('body').on('keydown', this.handleKeyDown.bind(this))
+	}
+	handleKeyUp(){
+		switch(event.key){
+			case 'ArrowDown':
+			case 'ArrowLeft':
+			case 'ArrowRight':
+			case 'ArrowUp':
+				this.thrust.y = 0;
+				this.thrust.x = 0;
+				break;
+		}
+	}
+	handleKeyDown(event){
+		switch(event.key){
+			case 'ArrowDown':
+				this.thrust.y = 1;
+				break;
+			case 'ArrowLeft':
+				this.thrust.x = -1;
+				break;
+			case 'ArrowRight':
+				this.thrust.x = 1;
+				break;
+			case 'ArrowUp':
+				this.thrust.y = -1;
+				break;
+		}
+	}
+	addThrust(){
+		this.motion.x += this.thrust.x * this.thrustPerInterval
+		this.motion.y += this.thrust.y * this.thrustPerInterval
+	}
+	move(){
+		var position = this.domElements.station.position();
+		this.domElements.station.css({
+			left: position.left + this.motion.x + 'px',
+			top: position.top + this.motion.y + 'px'
+		})
+	}
 	startUpdater(){
 		if(this.updateTimer!==null){
 			this.stopUpdater();
@@ -21,7 +73,24 @@ class StationSim{
 		clearInterval(this.updateTimer);
 		this.updateTimer = null;
 	}
+	processUpdate(){
+		this.addThrust();
+		this.move();
+		console.log(this.motion)
+	}
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
